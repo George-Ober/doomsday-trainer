25fe47f3d81d4a7f8853e30608e6598e0c1549c5
George Ober <89970387+George-Ober@users.noreply.github.com>
2024-07-08 21:04:46 +0200

    Changed scope

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline-v3";
+const CACHE = "github-pages-build5";
 
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
