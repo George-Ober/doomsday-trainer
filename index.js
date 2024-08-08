8db584cd53edf0538852f854b50cc15eb988a9a2
George Ober <89970387+George-Ober@users.noreply.github.com>
2024-08-08 20:41:08 +0200

    Added data dashboard powered by chart.js

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ let randomTime;
 let randomDate;
 let startTimestamp;
 let rightAnswer;
+let myChart;
 
 let awaitsInput = false;
 
@@ -293,11 +294,6 @@ gbid("next").addEventListener("click", () => {
 
 function downloadData() {
   let data = readLog();
-  if (localStorage.getItem("exercice-log") == null) {
-    data = [];
-  } else {
-    data = JSON.parse(localStorage.getItem("exercice-log"));
-  }
 
   const jsonString = JSON.stringify(data, null, 2);
 
@@ -312,4 +308,173 @@ function downloadData() {
   a.click();
   document.body.removeChild(a);
 }
-gbid("download").addEventListener("click", downloadData);
+gbid("download_btn").addEventListener("click", downloadData);
+
+let _modal = gbid("my_modal");
+
+let _stats_btn = gbid("open_stats");
+
+let _close_btn = gbid("close_btn");
+
+_stats_btn.onclick = function () {
+  document.body.style.overflow = "hidden";
+  _modal.classList.add("open");
+  _modal.style.display = "block";
+  document.querySelector(".modal-content").classList.add("open");
+
+  const data = readLog();
+
+  // Worst day
+  let worstDayCounters = [0, 0, 0, 0, 0, 0, 0];
+  let centuryCounters = [0, 0, 0, 0];
+
+  let correctTimes = [];
+  let q = 1;
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if (element.expectedAnswer === element.givenAnswer) {
+      correctTimes.push({
+        x: q,
+        y: parseFloat(element.timeTaken),
+        date: element.date,
+        playedAt: new Date(element.playedAt).toLocaleString(),
+      });
+      q++;
+
+      let century = Math.floor(
+        parseInt(element.date.match(/(\d{4})-\d{2}-\d{2}/)[0]) / 100
+      );
+      centuryCounters[century % 4] += 1;
+    } else {
+      worstDayCounters[element.expectedAnswer] += 1;
+    }
+  }
+
+  let worstDay = findHighestValueAndPercentage(worstDayCounters);
+  gbid("worstday").innerText = daysOfWeek[worstDay.highestIndex];
+  gbid("worstday_failures").innerText = `with ${
+    worstDayCounters[worstDay.highestIndex]
+  } failures`;
+
+  gbid("number_games").innerText = data.length;
+  gbid("number_succeeded").innerText = correctTimes.length;
+
+  let bestCentury =
+    correctTimes.length >= 1
+      ? findHighestValueAndPercentage(centuryCounters)
+      : { highestIndex: 0, percentage: "0%" };
+  gbid("preferred_century").innerText = `${
+    ((bestCentury.highestIndex + 2) % 4) + 18
+  }00s`;
+  gbid(
+    "preferred_century_percentage"
+  ).innerText = `with on avg. ${bestCentury.percentage} better accuracy`;
+
+  if (correctTimes.length >= 2) {
+    const lastTenAverages = [];
+
+    for (let i = 0; i < correctTimes.length; i++) {
+      if (i < 10) {
+        // Calculate the average of all items since the beginning
+        const sum = correctTimes
+          .slice(0, i + 1)
+          .reduce((acc, val) => acc + val.y, 0);
+        lastTenAverages[i] = { x: i + 1, y: sum / (i + 1) };
+      } else {
+        // Calculate the average of items from i - 10 to i
+        const sum = correctTimes
+          .slice(i - 9, i + 1)
+          .reduce((acc, val) => acc + val.y, 0);
+        lastTenAverages[i] = { x: i + 1, y: sum / 10 };
+      }
+    }
+
+    gbid("avg10").innerText = `${
+      Math.round(lastTenAverages[lastTenAverages.length - 1].y * 10) / 10
+    }s`;
+
+    // Get the canvas element
+    var ctx = document.getElementById("my_chart").getContext("2d");
+
+    // Create the chart
+    myChart = new Chart(ctx, {
+      type: "scatter",
+      data: {
+        datasets: [
+          {
+            label: "Times when Succeeded",
+            data: correctTimes,
+            type: "scatter",
+            tooltip: {
+              callbacks: {
+                beforeLabel: function (context) {
+                  return `Time: ${context.raw.y}`;
+                },
+                afterLabel: function (context) {
+                  return `Date: ${context.raw.date}`;
+                },
+                label: function (context) {
+                  return `Played At: ${context.raw.playedAt}`;
+                },
+              },
+            },
+          },
+          {
+            label: "Average of 10",
+            data: lastTenAverages,
+            type: "line",
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "linear",
+            display: false,
+          },
+          y: {
+            type: "linear",
+          },
+        },
+        plugins: {
+          tooltip: {
+            mode: "nearest",
+            intersect: false,
+          },
+        },
+      },
+    });
+  }
+};
+function findHighestValueAndPercentage(arr) {
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+
+  const highestIndex = arr.indexOf(Math.max(...arr));
+
+  const highestValue = arr[highestIndex];
+  const percentage = Math.round((highestValue / sum) * 100);
+
+  return { highestIndex, percentage: `${percentage}%` };
+}
+
+function closeModal() {
+  document.body.style.overflow = "auto";
+  _modal.classList.add("close");
+  document.querySelector(".modal-content").classList.add("close");
+  setTimeout(function () {
+    _modal.style.display = "none";
+    _modal.classList.remove("open", "close");
+    document.querySelector(".modal-content").classList.remove("open", "close");
+  }, 300);
+  myChart.destroy();
+}
+
+_close_btn.onclick = function () {
+  closeModal();
+};
+
+window.onclick = function (event) {
+  if (event.target == _modal) {
+    closeModal();
+  }
+};
